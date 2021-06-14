3b52c3b730a345c7a83aca616fa8f8f6aa924af3
SamnangThorn <samnang.th123n@gmail.com>
2021-06-14 13:51:40 -0400

    finished sign up

--- app/src/main/java/com/samnangthorn/hatt_app10/SignUp.java
+++ app/src/main/java/com/samnangthorn/hatt_app10/SignUp.java
@@ -1,14 +1,24 @@
 package com.samnangthorn.hatt_app10;
 
+import androidx.annotation.BinderThread;
+import androidx.annotation.ColorRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
@@ -43,7 +53,7 @@ public class SignUp extends AppCompatActivity {
         btt_signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signUp(email, password1);
+                signUp(email, password1, userName, password2);
             }
         });
 
@@ -67,9 +77,62 @@ public class SignUp extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void signUp(TextInputLayout email, TextInputLayout password1) {
-        String Email = email.getEditText().toString();
-        String Password = password1.getEditText().toString();
+    public void signUp(TextInputLayout email, TextInputLayout password1, TextInputLayout userName, TextInputLayout password2) {
+        String Email = email.getEditText().getText().toString();
+        String Password = password1.getEditText().getText().toString();
+        String confirmPassword = password2.getEditText().getText().toString();
+        String Username = userName.getEditText().getText().toString();
+
+        if (!Email.isEmpty() && !Password.isEmpty() && !confirmPassword.isEmpty() && !Username.isEmpty()) {
+            if (Patterns.EMAIL_ADDRESS.matcher(Email).matches() && Username.length() < 15 && confirmPassword.equals(Password)) {
+                email.setError(null);
+                password2.setError(null);
+                if (Password.length() >= 12) {
+                    password1.setError(null);
+                    boolean conditionTrue = false;
+                    char currentChar;
+                    for (int x = 0; x < Password.length(); x++) {
+                        currentChar = Password.charAt(x);
+                        if (currentChar > 47 && currentChar < 58) {
+                            conditionTrue = true;
+                            break;
+                        }
+                    }
+                    if (conditionTrue) {
+                        password1.setError(null);
+                        // Google Authentication Sign Up new User
+                        mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(SignUp.this, "Sign Up Successful!", Toast.LENGTH_SHORT).show();
+                                open_homeLayout();
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(SignUp.this, "Sign Up Failed!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        password1.setError("*Must include at least 1 number and 12 characters long");
+                    }
+                }else{
+                    password1.setError("*Must include at least 1 number and 12 characters long");
+                }
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(Email).matches()) {
+                email.setError("Incorrect email Format!");
+            } else {
+                password2.setError("Re-enter your new password");
+            }
+        } else {
+            Toast.makeText(SignUp.this, "Empty field Found!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void open_homeLayout() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 
 }
