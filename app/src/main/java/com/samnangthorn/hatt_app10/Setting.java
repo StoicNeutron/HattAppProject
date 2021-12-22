6018c1104fdf0b3049acd62b37866252c1123cdc
SamnangThorn <samnang.th123n@gmail.com>
2021-12-22 14:04:50 -0500

    v1.1_backend bmi2

--- app/src/main/java/com/samnangthorn/hatt_app10/Setting.java
+++ app/src/main/java/com/samnangthorn/hatt_app10/Setting.java
@@ -31,8 +31,6 @@ public class Setting extends AppCompatActivity {
     private Button save_btt;
     private LinearLayout weight, height, edt_restTime, btt_signOut, unit, edt_weight, edt_height;
     private TextView email, txt_restTime, txt_unit, txt_the_weight, txt_the_height;
-    private FirebaseAuth mAuth;
-    private FirebaseFirestore firebase_database;
     private SharedPreferences getData, getData2;
     private SharedPreferences.Editor editData, editData2;
     private Dialog logOut_dialog, weight_dialog, height_dialog, restTime_dialog, profile_dialog;
@@ -92,6 +90,8 @@ public class Setting extends AppCompatActivity {
         btt_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth mAuth = null;
+                FirebaseFirestore firebase_database;
                 mAuth.getInstance().signOut();
                 getData2 = getApplicationContext().getSharedPreferences("workout_data", MODE_PRIVATE);
                 editData2 = getData2.edit();
@@ -111,13 +111,20 @@ public class Setting extends AppCompatActivity {
         weight_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         weight_dialog.setCancelable(false);
 
-        TextView btt_weightDialog_cancel, btt_weightDialog_save;
+        TextView btt_weightDialog_cancel, btt_weightDialog_save, lbOrKg;
         NumberPicker WeightPicker1, WeightPicker2, WeightPicker3;
         btt_weightDialog_cancel = weight_dialog.findViewById(R.id.btt_cancel2);
         btt_weightDialog_save = weight_dialog.findViewById(R.id.btt_save);
+        lbOrKg = weight_dialog.findViewById(R.id.lbOrKg);
         WeightPicker1 = weight_dialog.findViewById(R.id.WeightPicker1);
         WeightPicker2 = weight_dialog.findViewById(R.id.WeightPicker2);
         WeightPicker3 = weight_dialog.findViewById(R.id.WeightPicker3);
+
+        if(Unit.equalsIgnoreCase("NonUS")){
+            lbOrKg.setText("Kg");
+        }else{
+            lbOrKg.setText("lb");
+        }
 
         btt_weightDialog_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -137,13 +144,13 @@ public class Setting extends AppCompatActivity {
                     String newWeight_US = String.valueOf(inputWeight1 + inputWeight2 + inputWeight3);
                     editData.putString("weight_US", newWeight_US);
                     double newWeight_NonUS = ((inputWeight1 + inputWeight2 + inputWeight3)/2.205);
-                    editData.putString("weight_NonUS", String.valueOf(newWeight_NonUS));
+                    editData.putString("weight_NonUS", String.format("%.0f", newWeight_NonUS));
                     txt_the_weight.setText(newWeight_US);
                 }else{
                     String newWeight_NonUS = String.valueOf(inputWeight1 + inputWeight2 + inputWeight3);
                     editData.putString("weight_NonUS", newWeight_NonUS);
                     double newWeight_US = ((inputWeight1 + inputWeight2 + inputWeight3)*2.205);
-                    editData.putString("weight_NonUS", String.valueOf(newWeight_US));
+                    editData.putString("weight_US", String.format("%.0f", newWeight_US));
                     txt_the_weight.setText(newWeight_NonUS);
                 }
 
@@ -171,13 +178,20 @@ public class Setting extends AppCompatActivity {
         height_dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         height_dialog.setCancelable(false);
 
-        TextView btt_heightDialog_cancel, btt_heightDialog_save;
+        TextView btt_heightDialog_cancel, btt_heightDialog_save, ftOrM;
         NumberPicker Height_picker1, Height_picker2, Height_picker3;
         btt_heightDialog_cancel = height_dialog.findViewById(R.id.btt_cancel3);
         btt_heightDialog_save = height_dialog.findViewById(R.id.btt_save3);
+        ftOrM = height_dialog.findViewById(R.id.ftOrM);
         Height_picker1 = height_dialog.findViewById(R.id.Height_picker1);
         Height_picker2 = height_dialog.findViewById(R.id.Height_picker2);
         Height_picker3 = height_dialog.findViewById(R.id.Height_Picker3);
+
+        if(Unit.equalsIgnoreCase("NonUS")){
+            ftOrM.setText("M");
+        }else{
+            ftOrM.setText("ft");
+        }
 
         btt_heightDialog_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -198,14 +212,15 @@ public class Setting extends AppCompatActivity {
                     int inputHeight_US = (inputHeight1*12) + (inputHeight2*10 + inputHeight3);
                     editData.putString("height_US", String.valueOf(inputHeight_US));
                     double inputHeight_NonUS = inputHeight_US/39.37;
-                    editData.putString("height_NonUS", String.valueOf(inputHeight_NonUS));
+                    //String. format("%. 2f", 1.23456)
+                    editData.putString("height_NonUS", String.format("%.2f", inputHeight_NonUS));
                     txt_the_height.setText(inputHeight1 + "." + ((inputHeight2*10) + inputHeight3));
                 }else{
                     // save as meters
                     String inputHeight_NonUS = inputHeight1 + "."+ (inputHeight2*10) + inputHeight3;
                     editData.putString("height_NonUS", inputHeight_NonUS);
                     double inputHeight_US = Double.valueOf(inputHeight_NonUS)*39.37;
-                    editData.putString("height_US", String.valueOf(inputHeight_US));
+                    editData.putString("height_US", String.format("%.2f", inputHeight_US));
                     txt_the_height.setText(inputHeight_NonUS);
                 }
 
@@ -327,12 +342,20 @@ public class Setting extends AppCompatActivity {
         newUserName = getData.getString("user_name", "ERROR");
         txt_userName.setText(newUserName);
         email.setText(getData.getString("email_address", "ERROR"));
-        newWeight = getData.getString("weight", "ERROR");
+
+        Double displayHeight;
+        if(Unit.equalsIgnoreCase("US")){
+            newHeight = getData.getString("height_US", "0");
+            displayHeight = Double.valueOf(newHeight)/12;
+            newWeight = getData.getString("weight_US", "0");
+        }else{
+            newHeight = getData.getString("height_NonUS", "0");
+            displayHeight = Double.valueOf(newHeight)*3.281;
+            newWeight = getData.getString("weight_NonUS", "0");
+        }
+        txt_the_height.setText(String.format("%.2f", displayHeight));
         txt_the_weight.setText(newWeight);
-        //weight.setText(newWeight);
-        newHeight = getData.getString("height", "ERROR");
-        txt_the_height.setText(newHeight);
-        //height.setText(newHeight);
+
         newTime = getData.getString("rest_time", "60");
         txt_restTime.setText(newTime);
         //profile
@@ -452,12 +475,22 @@ public class Setting extends AppCompatActivity {
                     Toast.makeText(Setting.this, "US Unit Switched", Toast.LENGTH_SHORT).show();
                     Unit = "US";
                     txt_unit.setText("LB-FT");
+                    //
+                    txt_the_weight.setText(getData.getString("weight_US", "0"));
+                    newHeight = getData.getString("height_US", "0");
+                    Double displayHeight = Double.valueOf(newHeight)/12;
+                    txt_the_height.setText(String.format("%.2f", displayHeight));
                 }else if(Unit.equalsIgnoreCase("US")){
                     editData.putString("unit", "NonUS");
                     editData.apply();
                     Toast.makeText(Setting.this, "NonUS Unit Switched", Toast.LENGTH_SHORT).show();
                     Unit = "NonUS";
                     txt_unit.setText("KG-M");
+                    //
+                    txt_the_weight.setText(getData.getString("weight_NonUS", "0"));
+                    newHeight = getData.getString("height_NonUS", "0");
+                    Double displayHeight = Double.valueOf(newHeight)*3.281;
+                    txt_the_height.setText(String.format("%.2f", displayHeight));
                 }
             }
         });
